9a494d7ec2a412ec49ccd394051d0b6cfd72502a
Mitesh Verma <miteshverma989@gmail.com>
2025-01-09 02:12:26 +0530

    added top bar

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,16 @@
+
+
+const Topbar = () => {
+  return (
+    <div>
+      <div className="bg-rose-600 px-4 py-3 text-white">
+  <p className="text-center text-sm font-medium">
+    Love Alpine JS?
+    <a href="#" className="inline-block underline">Check out this new course!</a>
+  </p>
+</div>
+    </div>
+  )
+}
+
+export default Topbar
